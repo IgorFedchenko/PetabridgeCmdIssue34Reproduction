6e35f3f1641299609448cd4445d63b3c8a9f43fb
IgorFedchenko <igor@fedchenko.pro>
2020-10-28 20:15:42 +0300

    Added reproduction project running on 9111 port

--- PetabridgeCmdIssue34Reproduction/Program.cs
+++ PetabridgeCmdIssue34Reproduction/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace PetabridgeCmdIssue34Reproduction
+﻿using System.Configuration;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Configuration;
+using Akka.Configuration.Hocon;
+using Petabridge.Cmd.Host;
+
+namespace PetabridgeCmdIssue34Reproduction
 {
   internal class Program
   {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
+        var section = (AkkaConfigurationSection)ConfigurationManager.GetSection("akka");
+        var actorSys = ActorSystem.Create("MyActorSystem", section.AkkaConfig);
+
+        PetabridgeCmd.Get(actorSys).Start();
+
+        await actorSys.WhenTerminated;
     }
   }
 }
